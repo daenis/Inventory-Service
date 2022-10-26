97358e9a0f7ef2406a1dfe333128da04d84033ab
jenki2ma <94399803+jenki2ma@users.noreply.github.com>
2022-10-25 23:47:00 -0400

    Schema added

---
+++ src/main/java/com/autoshop/inventoryservice/core/DatabaseConfiguration.java
@@ -0,0 +1,30 @@
+package com.autoshop.inventoryservice.core;
+
+import org.apache.catalina.servlets.WebdavServlet;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class DatabaseConfiguration {
+
+    @Bean
+    ServletRegistrationBean h2ServletRegistration() {
+        ServletRegistrationBean registrationBean = new ServletRegistrationBean(new WebdavServlet());
+        registrationBean.addUrlMappings("/console/*");
+        return registrationBean;
+    }
+
+    @Bean
+    public DataSource dataSource() {
+        return new EmbeddedDatabaseBuilder()
+                .setType(EmbeddedDatabaseType.H2)
+                .addScripts("schema.sql")
+                .build();
+    }
+}
---
+++ src/main/resources/schema.sql
@@ -0,0 +1,27 @@
+CREATE TABLE IF NOT EXISTS public.auto_part_types
+(
+  id integer NOT NULL,
+  name text COLLATE pg_catalog."default" NOT NULL,
+  CONSTRAINT auto_part_types_pkey PRIMARY KEY (id)
+);
+
+TABLESPACE pg_default;
+
+ALTER TABLE IF EXISTS public.auto_part_types OWNER to postgres;
+
+CREATE TABLE IF NOT EXISTS public.auto_parts
+(
+  id integer NOT NULL,
+  auto_part_type_id integer NOT NULL,
+  CONSTRAINT auto_parts_pkey PRIMARY KEY (id),
+  CONSTRAINT auto_parts_auto_part_type_id_fkey FOREIGN KEY (auto_part_type_id)
+    REFERENCES public.auto_part_types (id) MATCH SIMPLE
+    ON UPDATE NO ACTION
+    ON DELETE NO ACTION
+);
+
+TABLESPACE pg_default;
+
+ALTER TABLE IF EXISTS public.auto_parts OWNER to postgres;
+
+INSERT INTO auto_part_types (id, name) VALUES (1, 'Engine');
