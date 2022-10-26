31d988f1ea1c91ef3b7a761d09eaab279f5c1bec
daenis <dkalaygia@gmail.com>
2022-10-25 16:42:28 -1000

    First commit

---
+++ src/main/java/com/autoshop/inventoryservice/App.java
@@ -0,0 +1,13 @@
+package com.autoshop.inventoryservice;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
